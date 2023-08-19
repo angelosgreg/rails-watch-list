a49e29828e10f62db26ca9d828fe550164950993
Angelos Grigorakis <angelosgreg13@gmail.com>
2023-08-19 11:24:56 +0200

    rails watch list

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,8 @@
 
 import { application } from "./application"
 
-import TomSelectController from "./tom_select_controller.js"
-application.register("tom-select", TomSelectController)
+// import TomSelectController from "./tom_select_controller.js"
+// application.register("tom-select", TomSelectController)
 
-import StarRatingController from "./star_rating_controller.js"
-application.register("star-rating", StarRatingController)
+// import StarRatingController from "./star_rating_controller.js"
+// application.register("star-rating", StarRatingController)
